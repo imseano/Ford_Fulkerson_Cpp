85e0b3be60fb7384992f64fce4fb1f2d99285d16
imseano <soreta@csu.fullerton.edu>
2023-04-07 11:48:39 -0700

    Running code setup:

--- main.cpp
+++ main.cpp
@@ -15,9 +15,28 @@
 //    Must be run alongside intarray.asm
 
 #include <iostream>
+#include <vector>
+#include <queue>
+
+
+int fordFulkerson(std::vector<std::vector<int>> graph, int start, int end) {
+ int ans = 0;
+
+ return ans;
+}
 
 int main() {
-    printf("hi");
+    std::vector<std::vector<int>> graph{
+                { 0, 0, 0, 0, 0 },
+                { 0, 0, 0, 0, 0 },
+                { 0, 0, 0, 0, 0 },
+                { 0, 0, 0, 0, 0 }
+            };
+    printf("hi\n");
+    int maxFlow = fordFulkerson(graph, 0, 1);
+    printf("The max flow for this system is: %d\n", maxFlow);
     return 0;
 }
+
+
 
