1d32d0ad3f38d17d56e56311e575768d68cbf22b
imseano <soreta@csu.fullerton.edu>
2023-04-09 16:23:55 -0700

    Added BFS

--- main.cpp
+++ main.cpp
@@ -8,11 +8,37 @@
 #include <vector>
 #include <queue>
 
+// BFS Search Method
+bool BFS(std::vector<std::vector<int>> graph, int start, int end, std::vector<int> parent){
+    bool visited[graph.size()];
+    std::queue<int> Q;
+    Q.push(start);
+    visited[start] = true;
+    parent[start] = -1;
+    while(!Q.empty()) {
+    	int node = Q.front();
+	Q.pop();
+	for (size_t n = 0; n != graph.size(); n++) {
+	    if (!visited[n] && graph[node][n] > 0) {
+	    	Q.push(n);
+		parent[n] = node;
+		visited[n] = true;
+	    }
+	}
+    }
+    return (visited[end] = true);
+}
 
 int fordFulkerson(std::vector<std::vector<int>> graph, int start, int end) {
- int ans = 0;
+ int max_flow = 0;
 
- return ans;
+ std::vector<std::vector<int>> rgraph = graph;
+
+ std::vector<int> parent(graph.size());
+ while (BFS(rgraph, start, end, parent)) {
+    break;
+ }
+ return max_flow;
 }
 
 int main() {
