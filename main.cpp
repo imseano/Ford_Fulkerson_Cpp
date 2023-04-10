4c882ed9dbcf8c4de63a6d305cedf46ac7636e04
imseano <soreta@csu.fullerton.edu>
2023-04-09 17:27:45 -0700

    Working code

--- main.cpp
+++ main.cpp
@@ -7,10 +7,17 @@
 #include <iostream>
 #include <vector>
 #include <queue>
+#include <climits>
+
+std::vector<int> parent; // Used to store parents of nodes later.
 
 // BFS Search Method
-bool BFS(std::vector<std::vector<int>> graph, int start, int end, std::vector<int> parent){
+bool BFS(std::vector<std::vector<int>> graph, int start, int end){
     bool visited[graph.size()];
+    for (size_t x = 0; x < graph.size(); x++) {
+    	visited[x] = false;
+	parent[x] = false;
+    }
     std::queue<int> Q;
     Q.push(start);
     visited[start] = true;
@@ -19,36 +26,51 @@ bool BFS(std::vector<std::vector<int>> graph, int start, int end, std::vector<in
     	int node = Q.front();
 	Q.pop();
 	for (size_t n = 0; n != graph.size(); n++) {
-	    if (!visited[n] && graph[node][n] > 0) {
+	    if (visited[n] == false && graph[node][n] > 0) {
 	    	Q.push(n);
 		parent[n] = node;
 		visited[n] = true;
 	    }
 	}
     }
-    return (visited[end] = true);
+    return (visited[end] == true);
 }
 
 int fordFulkerson(std::vector<std::vector<int>> graph, int start, int end) {
  int max_flow = 0;
-
+ // Residual Graph
  std::vector<std::vector<int>> rgraph = graph;
 
- std::vector<int> parent(graph.size());
- while (BFS(rgraph, start, end, parent)) {
-    break;
+ parent.resize(graph.size());
+ while (BFS(rgraph, start, end)) {
+    int flow = INT_MAX;
+    int current_node;
+    for (int x = end; x != start; x = parent[x]) {
+    	current_node = parent[x];
+	flow = std::min(flow, rgraph[current_node][x]);
+    }
+    for (int x = end; x != start; x = parent[x]) {
+    	current_node = parent[x];
+	rgraph[current_node][x] -= flow;
+	rgraph[x][current_node] += flow;
+    }
+    max_flow += flow;
+    
  }
  return max_flow;
 }
 
 int main() {
+    // Graph is saved as an adjacency matrix.
     std::vector<std::vector<int>> graph{
-                { 0, 0, 0, 0, 0 },
-                { 0, 0, 0, 0, 0 },
-                { 0, 0, 0, 0, 0 },
-                { 0, 0, 0, 0, 0 }
+                { 0, 10, 0, 10, 0, 0 }, // S
+                { 0, 0, 4, 2, 8, 0}, 	// A
+                { 0, 0, 0, 0, 0, 10 },	// B
+                { 0, 0, 0, 0, 9, 0 },	// C
+                { 0, 0, 6, 0, 0, 10 },	// D
+                { 0, 0, 0, 0, 0 }	// T 
             };
-    int maxFlow = fordFulkerson(graph, 0, 1);
+    int maxFlow = fordFulkerson(graph, 0, 5);
     printf("The max flow for this system is: %d\n", maxFlow);
     return 0;
 }
